88ba470b6ff247e063db462fccff4ee6aaf52bab
MubasharHussain0324 <mubasharhussain13@yahoo.com>
2018-01-08 09:13:14 +0500

    Initial commit

---
+++ app/src/main/java/com/example/mubasharhussain/okhttp/CustomEvent.java
@@ -0,0 +1,8 @@
+package com.example.mubasharhussain.okhttp;
+
+/**
+ * Created by MubasharHussain on 1/8/2018.
+ */
+
+class CustomEvent {
+}
---
+++ app/src/main/java/com/example/mubasharhussain/okhttp/CutomEvent.java
@@ -0,0 +1,8 @@
+package com.example.mubasharhussain.okhttp;
+
+/**
+ * Created by MubasharHussain on 1/7/2018.
+ */
+
+public class CutomEvent {
+}
